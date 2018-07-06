2a664eb268c08f2c4df0775866616589787cb558
s1250068-ogawa <s1250068@u-aizu.ac.jp>
2018-07-06 12:50:52 +0900

    Won/lost message modified

--- dice.c
+++ dice.c
@@ -4,6 +4,11 @@
 
 int main(){
 
+  char name[10];
+  printf("What is your name?\n>");
+  scanf("%s",name);
+  printf("Hello, %s!\n", name);
+
   srand((unsigned)time(NULL));
   int d1 = rand()%6+1;
   int d2 = rand()%6+1;
@@ -13,6 +18,9 @@ int main(){
   printf("Die 1: %d\n", d1);
   printf("Die 2: %d\n", d2);
   printf("Total value: %d\n", total);
+
+  if( total > 7 ) printf("%s won\n", name);
+  else printf("%s lost\n", name);
 
   return 0;
 }
